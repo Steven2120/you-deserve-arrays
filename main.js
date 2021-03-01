defc9b7a8ebd6338c8ff106296902080cde61826
Steven Moyano <stevenmoyano1@gmail.com>
2021-03-01 13:20:51 -0600

    Passed all tests

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function getFirstItemFrom(arr) {
+  return arr[0];
+}
 
+function getLastItemFrom(arr) {
+  return arr[arr.length -1];
+}
+
+function getIndex3(arr) {
+  if (arr.length <= 3) {
+    return arr[arr.length - 1];
+  } else {
+    return arr[3];
+  }
+}
+
+function isLongList(arr) {
+  if (arr.length >= 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function firstItemIsNumber(arr) {
+  if (typeof(arr[0]) === "number") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function secondCharOfThirdString(arr) {
+  return arr[2][1];
+}
 
 
 
